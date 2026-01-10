1196be2668a451177d0a8de8bcfca05d03115622
JuanPabloSE <juancontas@protonmail.com>
2026-01-09 22:13:11 -0300

    Commit Message

---
+++ src/ExProposto04.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExProposto04 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        double[] vet = new double[N];
+
+        for (int i = 0; i < N; i++) {
+            vet[i] = sc.nextDouble();
+        }
+
+        // 1: Calculando e mostrando a media
+        double soma = 0.0;
+        for (int i = 0; i < N; i++) {
+            soma += vet[i];
+        }
+
+        double media = soma / N;
+        System.out.printf("%.3f%n", media);
+
+        // 2: Mostrando todos elementos abaixo da media
+        for (int i = 0; i < N; i++) {
+            if (vet[i] < media) {
+                System.out.printf("%.1f%n", vet[i]);
+            }
+
+            sc.close();
+        }
+    }
+}
