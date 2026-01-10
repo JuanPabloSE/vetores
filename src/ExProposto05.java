a34f6bdd5ec2bbad05546486a3f9932a2c2541cc
JuanPabloSE <juancontas@protonmail.com>
2026-01-09 22:29:57 -0300

    Commit Message

--- src/ExProposto05.java
+++ src/ExProposto05.java
@@ -6,7 +6,30 @@ public class ExProposto05 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int N = sc.nextInt();
+        int [] vet = new int [N];
 
+        for (int i = 0; i < N; i++) {
+            vet[i] = sc.nextInt();
+        }
+
+        int soma = 0;
+        int cont = 0;
+        for (int i = 0; i < N; i++) {
+            if (vet[i] % 2 == 0) {
+            soma += vet[i];
+            cont++;
+            }
+        }
+
+        if (cont == 0) {
+            System.out.println("Nao havia nenhum numero par");
+        }
+
+        else {
+        double media = (double) soma / cont;
+        System.out.printf("%.1f%n", media);
+        }
 
         sc.close();
     }
