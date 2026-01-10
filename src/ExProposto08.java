0262b7b660755b9779dbd11f81f250d424f243e5
JuanPabloSE <juancontas@protonmail.com>
2026-01-10 19:35:14 -0300

    Commit Message

---
+++ src/ExProposto08.java
@@ -0,0 +1,63 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExProposto08 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        double [] altura = new double[N];
+        char [] sexo = new char [N];
+
+        for (int i = 0; i < N; i++) {
+            altura[i] = sc.nextDouble();
+            sexo[i] = sc.next().charAt(0);
+        }
+
+        double maior = altura[0];       // MAIOR ALTURA
+        for (int i = 1; i < N; i++) {
+            if (altura[i] > maior) {
+                maior = altura[i];
+            }
+        }
+        System.out.printf("Maior altura = %.2f%n", maior);
+
+        double menor = altura[0];       // MENOR ALTURA
+        for (int i = 1; i < N; i++) {
+            if (altura[i] < menor) {
+                menor = altura[i];
+            }
+        }
+        System.out.printf("Menor altura = %.2f%n", menor);
+
+        double somaMulheres = 0.0;
+        int cont = 0;
+        for (int i = 0; i < N; i++) {
+            if (sexo[i] == 'F') {
+                cont++;
+                somaMulheres += altura[i];
+            }
+        }
+
+        if (cont == 0) {
+            System.out.println("Nao ha nenhuma mulher dentre as pessoas");
+        }
+
+        else {
+        double mediaMulheres = somaMulheres / cont;
+            System.out.printf("Media das alturas das mulheres = %.2f%n", mediaMulheres);
+        }
+
+        int contHomens = 0;
+        for (int i = 0; i < N; i++) {
+            if (sexo[i] == 'M') {
+                contHomens++;
+            }
+        }
+        System.out.printf("Numero de homens = %d%n", contHomens);
+
+        sc.close();
+    }
+}
