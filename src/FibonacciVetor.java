70eb03404c864e36f3cc478166382d80da4a6c51
JuanPabloSE <juancontas@protonmail.com>
2026-02-06 11:45:56 -0300

    Commit Message

--- src/FibonacciVetor.java
+++ src/FibonacciVetor.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
-public class FibonnaciVetor {
+public class FibonacciVetor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite um termo/número: ");
         int N = sc.nextInt();
 
-        int [] fibonnaci = new int [N];
+        int [] fibonacci = new int [N];
 
-        fibonnaci[0] = 0;
+        fibonacci[0] = 0;
         if (N > 1) {
-            fibonnaci[1] = 1;
+            fibonacci[1] = 1;
         }
 
-        for (int i = 2; i < fibonnaci.length; i++) {
-            fibonnaci[i] = fibonnaci[i - 1] + fibonnaci[i - 2];
+        for (int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
         }
 
-        for (int ler : fibonnaci) {
+        for (int ler : fibonacci) {
             System.out.printf(ler + " ");
         }
 
